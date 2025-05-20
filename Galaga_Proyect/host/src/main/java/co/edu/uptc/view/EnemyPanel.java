2436c9427e95db5b39d4a8e8630b0726bcf673f9
lauramoreno8 <laura.moreno22@uptc.edu.co>
2025-05-20 14:53:16 -0500

    Primeras pantallas de la interfaz

---
+++ Galaga_Proyect/host/src/main/java/co/edu/uptc/view/EnemyPanel.java
@@ -0,0 +1,19 @@
+package co.edu.uptc.view;
+
+import javax.swing.JFrame;
+
+
+public class EnemyPanel extends JFrame {
+
+    public EnemyPanel() {
+        setTitle("Enemigos");
+        setSize(650, 500);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setLocationRelativeTo(null);
+        setLayout(null);
+        setResizable(false);
+        setVisible(true);
+    }
+
+
+}
